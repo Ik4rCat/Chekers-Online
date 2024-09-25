2639d4fa839a019b7d180c1a8e3fc7791fb6463c
Ik4rCat <119232131+Ik4rCat@users.noreply.github.com>
2024-09-25 17:57:09 +0300

    Спавн шашек всех, теперь без ошибок :)

--- Assets/Scripts/Board/BoardNetwork.cs
+++ Assets/Scripts/Board/BoardNetwork.cs
@@ -6,5 +6,17 @@ using UnityEngine;
 
 public class BoardNetwork : Board
 {
-    
+
+    private readonly SyncList<int[]> boardList = new SyncList<int[]>();
+    //теперь можно просто [SyncVar]
+
+
+    public override IList<int[]> BoardList => boardList;
+
+    public override void OnStartServer()
+    {
+        FillBoardList(boardList);
+    }
+
+
 }
