93c6957d396de0ea4326795122f66ad4cd597156
Ik4rCat <119232131+Ik4rCat@users.noreply.github.com>
2024-10-30 17:40:03 +0300

    исправлено

--- Assets/Scripts/Piece/PieceNetwork.cs
+++ Assets/Scripts/Piece/PieceNetwork.cs
@@ -5,5 +5,17 @@ using UnityEngine;
 
 public class PieceNetwork : NetworkBehaviour
 {
-    
+    [SyncVar(hook = nameof(SynchookSelfSetParent))]
+    PlayerPiecesHandler owner;
+
+    public override void OnStartServer()
+    {
+        owner = connectionToClient.identity.GetComponent<PlayerPiecesHandler>();
+    }
+
+    private void SynchookSelfSetParent(PlayerPiecesHandler oldOwner, PlayerPiecesHandler newOwner )
+    {
+        transform.parent = newOwner.PiecesParent;
+    }
+
 }
